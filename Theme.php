2d3d8b0ef4e94bf2b0a1eeda1e23a6a350f527f6
Evan Shaw <evandanielshaw@gmail.com>
2020-01-10 11:18:57 +0900

    fix: remove duplicate filter calls in Theme.php

--- Theme.php
+++ Theme.php
@@ -22,21 +22,17 @@ class Theme {
             case 'Welcart Basic':
                 $theme_config['name'] = 'basic';
                 $theme_config['color'] = apply_filters($hookprefix . '_tutils_color', 'orange');
-                $theme_config['color'] = apply_filters($hookprefix . '_tutils_color', 'orange');
                 break;
             case 'Welcart Nova':
                 $theme_config['name'] = 'nova';
-                $theme_config['color'] = apply_filters($hookprefix . '_tutils_color', 'red');
                 $theme_config['color'] = apply_filters($hookprefix . '_tutils_color', 'red');
                 break;
             case 'Welcart Default Theme':
                 $theme_config['name'] = 'default';
                 $theme_config['color'] = apply_filters($hookprefix . '_tutils_color', 'orange');
-                $theme_config['color'] = apply_filters($hookprefix . '_tutils_color', 'orange');
                 break;
             default:
                 $theme_config['name'] = apply_filters($hookprefix . '_tutils_default_name', '');
-                $theme_config['color'] = apply_filters($hookprefix . '_tutils_color', '');
                 $theme_config['color'] = apply_filters($hookprefix . '_tutils_color', '');
                 break;
         }
