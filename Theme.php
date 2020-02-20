0672395a46d7dce15e52201a64945448d4e54465
Evan Shaw <evandanielshaw@gmail.com>
2020-02-20 12:41:59 +0900

    feat: add Themes class for Welcart theming
    BREAKING CHANGES: removed deprecated Controller classes. Routing has moved to the package aivec/wordpress-router

--- Theme.php
+++ Theme.php
@@ -6,6 +6,8 @@ namespace Aivec\Welcart\Generic;
  */
 class Theme {
 
+    const VERSION = 'v3_0_0';
+
     /**
      * Returns config based on the currently enabled Welcart theme
      *
@@ -17,25 +19,56 @@ class Theme {
     public static function themeConfig($hookprefix) {
         $theme_config = [];
         $theme = wp_get_theme();
+        $theme_config['name'] = '';
+        $theme_config['btnclass'] = '';
         $theme_config['full_name'] = $theme->get('Name');
-        switch ($theme->get('Name')) {
-            case 'Welcart Basic':
+        $theme_config['stylesheet'] = $theme->stylesheet;
+        switch ($theme->stylesheet) {
+            case 'welcart_basic':
                 $theme_config['name'] = 'basic';
                 $theme_config['color'] = apply_filters($hookprefix . '_tutils_color', 'orange');
                 break;
-            case 'Welcart Nova':
+            case 'welcart_basic-nova':
                 $theme_config['name'] = 'nova';
                 $theme_config['color'] = apply_filters($hookprefix . '_tutils_color', 'red');
                 break;
-            case 'Welcart Default Theme':
-                $theme_config['name'] = 'default';
-                $theme_config['color'] = apply_filters($hookprefix . '_tutils_color', 'orange');
+            case 'welcart_basic-beldad':
+                $theme_config['name'] = 'beldad';
+                $theme_config['color'] = apply_filters($hookprefix . '_tutils_color', 'black');
+                break;
+            case 'welcart_basic-bordeaux':
+                $theme_config['name'] = 'bordeaux';
+                $theme_config['color'] = apply_filters($hookprefix . '_tutils_color', 'red');
+                break;
+            case 'welcart_basic-carina':
+                $theme_config['name'] = 'carina';
+                $theme_config['color'] = apply_filters($hookprefix . '_tutils_color', 'red');
+                break;
+            case 'welcart_basic-square':
+                $theme_config['name'] = 'square';
+                $theme_config['color'] = apply_filters($hookprefix . '_tutils_color', 'black');
+                break;
+            case 'welcart_basic-voll':
+                $theme_config['name'] = 'voll';
+                $theme_config['color'] = apply_filters($hookprefix . '_tutils_color', 'teal');
+                break;
+            case 'welcart_panetteria':
+                $theme_config['name'] = 'panetteria';
+                $theme_config['color'] = apply_filters($hookprefix . '_tutils_color', 'brown');
                 break;
             default:
                 $theme_config['name'] = apply_filters($hookprefix . '_tutils_default_name', '');
                 $theme_config['color'] = apply_filters($hookprefix . '_tutils_color', '');
                 break;
         }
+
+        if (strtolower($theme->get('Name')) === 'welcart default theme') {
+            $theme_config['name'] = 'default';
+            $theme_config['color'] = apply_filters($hookprefix . '_tutils_color', 'orange');
+        }
+
+        $theme_config['btnprimary'] = 'welbtn-' . self::VERSION . '-primary-' . $theme_config['name'];
+        $theme_config['btnsecondary'] = 'welbtn-' . self::VERSION . '-secondary-' . $theme_config['name'];
 
         return $theme_config;
     }
